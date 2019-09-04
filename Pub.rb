57a1bba471cfdfef640001ce46f51e4df0102bf4
cjwales <cjwales@gmail.com>
2019-09-04 12:01:35 +0100

    pub functions added

--- Pub.rb
+++ Pub.rb
@@ -8,4 +8,21 @@ class Pub
     @stock = []
 
   end
+
+  def add_funds_to_till(drink)
+    @till += drink.price()
+  end
+
+  def stock_amount()
+    return @stock.length()
+  end
+
+  def add_stock(drink)
+    @stock.push(drink)
+  end
+
+  def remove_stock(drink)
+    @stock.delete(drink)
+  end
+
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -1,12 +1,15 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../Pub.rb')
+require_relative('../Drinks.rb')
 
 class PubTest < MiniTest::Test
 
   def setup()
 
     @pub = Pub.new("The Dog & Duck", 100)
+    @wine = Drinks.new("Wine", 5)
+    @vodka = Drinks.new("Vodka", 2)
 
   end
 
@@ -19,4 +22,25 @@ class PubTest < MiniTest::Test
     assert_equal(100, @pub.till)
   end
 
+  def test_add_funds_to_till_balance()
+    @pub.add_funds_to_till(@wine)
+    assert_equal(105, @pub.till)
+  end
+
+  def test_check_stock_level()
+    assert_equal(0, @pub.stock_amount())
+  end
+
+  def test_add_drinks_to_stock()
+    @pub.add_stock(@wine)
+    @pub.add_stock(@vodka)
+    assert_equal(2, @pub.stock_amount())
+  end
+
+  def test_remove_stock()
+    @pub.add_stock(@wine)
+    @pub.add_stock(@vodka)
+    @pub.remove_stock(@wine)
+    assert_equal(1, @pub.stock_amount)
+  end
 end
